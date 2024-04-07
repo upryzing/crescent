b624df920304000a0d73e5dcb70d9bb8603f6f5d
bloom <val.perez@cemep.edu.do>
2024-04-06 22:00:40 -0400

    wip: Add MFA dialog, but has missing functionality
    Also, added App Name's Title on Login.kt

--- app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/navigation/Login.kt
+++ app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/navigation/Login.kt
@@ -1,17 +1,32 @@
 package org.nekoweb.amycatgirl.revolt.ui.navigation
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Input
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,15 +34,90 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import org.nekoweb.amycatgirl.revolt.R
 import org.nekoweb.amycatgirl.revolt.ui.theme.RevoltTheme
 
+@Composable
+fun MFADialog() {
+    var mfaValue by rememberSaveable { mutableStateOf("") }
+
+    Dialog(
+        onDismissRequest = { /*TODO*/ },
+    )
+    {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min),
+            shape = RoundedCornerShape(28.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+                contentColor = MaterialTheme.colorScheme.onSurface
+            )
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(24.dp)
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Filled.Input,
+                    "",
+                    modifier = Modifier
+                        .size(24.dp),
+                    tint = MaterialTheme.colorScheme.secondary,
+                )
+                Text(
+                    "MFA Required",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .wrapContentSize(Alignment.Center),
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Text(
+                    "Please enter your MFA code provided by your authenticator app",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    value = mfaValue,
+                    onValueChange = { mfaValue = it },
+                    label = { Text("MFA Code") }
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .padding(horizontal = 24.dp)
+                    .wrapContentSize(Alignment.CenterEnd),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Login")
+                }
+                TextButton(onClick = { /*TODO*/ }) {
+                    Text("Cancel")
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
 
 @Composable
 fun LoginPage() {
     var emailValue by rememberSaveable { mutableStateOf("") }
     var passwordValue by rememberSaveable { mutableStateOf("") }
+    AnimatedVisibility(visible = false) {
+        MFADialog()
+    }
     Scaffold { innerPadding ->
         Column(
             modifier = Modifier
@@ -36,6 +126,7 @@ fun LoginPage() {
             verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(stringResource(R.string.app_name), style = MaterialTheme.typography.headlineLarge)
             OutlinedTextField(
                 value = emailValue,
                 onValueChange = { emailValue = it },
@@ -60,6 +151,14 @@ fun LoginPage() {
 fun LoginPagePreview() {
     RevoltTheme {
         LoginPage()
+    }
+}
+
+@Preview
+@Composable
+fun MFADialogPreview() {
+    RevoltTheme {
+        MFADialog()
     }
 }
 
