cef605343a59603bf19aa3ad6dcbb66d5b45ab75
Amy <amy@amogus.cloud>
2024-04-26 15:59:43 -0400

    chore: remove starter composable
    Signed-off-by: Amy <amy@amogus.cloud>

--- app/src/main/java/org/nekoweb/amycatgirl/revolt/MainActivity.kt
+++ app/src/main/java/org/nekoweb/amycatgirl/revolt/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import org.nekoweb.amycatgirl.revolt.ui.theme.RevoltTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,21 +25,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RevoltTheme {
-        Greeting("Android")
     }
 }
