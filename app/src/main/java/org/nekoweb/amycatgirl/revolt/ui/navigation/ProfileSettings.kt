50acc6e5a942d640a22445e96c9d89bcb0371bd0
bloom <val.perez@cemep.edu.do>
2024-04-27 01:21:29 -0400

    feat: finalize AccountSettings design.
    Also, before I forget, ProfileSettings needs design at this moment.

---
+++ app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/navigation/ProfileSettings.kt
@@ -0,0 +1,100 @@
+package org.nekoweb.amycatgirl.revolt.ui.navigation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.nekoweb.amycatgirl.revolt.ui.theme.RevoltTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileSettingsPage() {
+    var userName by remember { mutableStateOf("") }
+//    TODO: Profile Settings Design 🙏
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Profile Settings")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            ListItem(
+                headlineContent = { Text("Username", modifier = Modifier.padding(end = 12.dp)) },
+                trailingContent = {
+                    OutlinedTextField(
+                        value = userName,
+                        placeholder = { Text("Username") },
+                        onValueChange = { userName = it },
+                        trailingIcon = {
+                            Text(
+                                "#1234",
+                                fontSize = 17.sp,
+                                modifier = Modifier.padding(end = 12.dp)
+                            )
+                        },
+                        modifier = Modifier.fillMaxWidth(.7f)
+                    )
+                })
+
+            ListItem(
+                headlineContent = { Text("Email", modifier = Modifier.padding(end = 12.dp)) },
+                supportingContent = { Text("example@domain.com") },
+                trailingContent = {
+                    Button(
+                        onClick = { /*TODO*/ },
+                    ) {
+                        Text("Change Email")
+                    }
+                })
+
+            ListItem(
+                headlineContent = { Text("Password", modifier = Modifier.padding(end = 12.dp)) },
+                supportingContent = { Text("●●●●●●●●") },
+                trailingContent = {
+                    Button(
+                        onClick = { /*TODO*/ },
+                    ) {
+                        Text("Change Password")
+                    }
+                })
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProfileSettingsPagePreview() {
+    RevoltTheme {
+        ProfileSettingsPage()
+    }
+}
