b68c86fd4ecd8b82ad8b2f4ec4b84c3d8d379f24
Amy <amy@amogus.cloud>
2024-06-21 21:05:14 -0400

    refactor: remove unused SocketListener interface

--- app/src/main/java/org/nekoweb/amycatgirl/revolt/models/api/websocket/SocketListener.kt
+++
@@ -1,7 +0,0 @@
-package org.nekoweb.amycatgirl.revolt.models.api.websocket
-
-interface SocketListener {
-    fun onConnect()
-    fun onSocketMessage(raw: String)
-    fun onDisconnect(error: Exception?)
-}
