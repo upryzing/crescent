0442c3f48958cb1404c331299bc1bf90edced95f
Bloom-69 <bloomdevelop@proton.me>
2024-06-22 14:14:29 -0400

    chore(home): remove unused imports

--- app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/navigation/Home.kt
+++ app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/navigation/Home.kt
@@ -1,15 +1,6 @@
 package org.nekoweb.amycatgirl.revolt.ui.navigation
 
 import android.util.Log
-import androidx.compose.animation.Crossfade
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.safeDrawingPadding
@@ -17,9 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MediumTopAppBar
@@ -28,10 +17,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -84,7 +71,7 @@ fun HomePage(
             )
         },
         floatingActionButton = {
-            FloatingActionButtonWithOptions( options = listOf(
+            FloatingActionButtonWithOptions(options = listOf(
                 FloatingActionButtonListItem(
                     icon = {
                         Icon(Icons.Outlined.Add, contentDescription = "Add")
