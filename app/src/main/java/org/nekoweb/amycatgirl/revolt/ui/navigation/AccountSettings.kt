06c05e5baac454b7dc882c1b7a966da4c0830f40
Bloom <129090449+bloomdevelop@users.noreply.github.com>
2024-09-24 09:33:55 -0400

    wip: new AccountSettingsPage design Not all needed feature are here or implement currently, will think about adding it later on

--- app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/navigation/AccountSettings.kt
+++ app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/navigation/AccountSettings.kt
@@ -1,36 +1,35 @@
 package org.nekoweb.amycatgirl.revolt.ui.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import org.nekoweb.amycatgirl.revolt.R
 import org.nekoweb.amycatgirl.revolt.ui.theme.RevoltTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AccountSettingsPage() {
-    var userName by remember { mutableStateOf("") }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -49,48 +48,59 @@ fun AccountSettingsPage() {
         }
     ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
-            ListItem(
-                headlineContent = { Text("Username", modifier = Modifier.padding(end = 12.dp)) },
-                trailingContent = {
-                    OutlinedTextField(
-                        value = userName,
-                        placeholder = { Text("Username") },
-                        onValueChange = { userName = it },
-                        trailingIcon = {
-                            Text(
-                                "#1234",
-                                fontSize = 17.sp,
-                                modifier = Modifier.padding(end = 12.dp)
-                            )
-                        },
-                        modifier = Modifier.fillMaxWidth(.7f)
-                    )
-                })
-
-            ListItem(
-                headlineContent = { Text("Email", modifier = Modifier.padding(end = 12.dp)) },
-                supportingContent = { Text("example@domain.com") },
-                trailingContent = {
-                    Button(
-                        onClick = { /*TODO*/ },
-                    ) {
-                        Text("Change Email")
+            Column(modifier = Modifier.padding(start = 12.dp, end = 12.dp), verticalArrangement = Arrangement.spacedBy(12.dp)) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(MaterialTheme.shapes.large)
+                        .clickable { /* TODO: Show the dialog here :) */ }
+                        .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+                        .padding(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                        Column {
+                            Text("Change Username")
+                            Text("Current username: dummy#1234", style = MaterialTheme.typography.bodySmall)
+                        }
+                        Icon(painterResource(R.drawable.material_symbols_edit), "")
                     }
-                })
-
-            ListItem(
-                headlineContent = { Text("Password", modifier = Modifier.padding(end = 12.dp)) },
-                supportingContent = {
-//                  TODO: Find a way to convert the password to a star character.
-                    Text("**********")
-                },
-                trailingContent = {
-                    Button(
-                        onClick = { /*TODO*/ },
-                    ) {
-                        Text("Change Password")
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(MaterialTheme.shapes.large)
+                        .clickable { /* TODO: Show the dialog here :) */ }
+                        .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+                        .padding(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                        Column {
+                            Text("Change Email")
+                            Text("Your current email: dummy@example.com", style = MaterialTheme.typography.bodySmall)
+                        }
+                        Icon(painterResource(R.drawable.material_symbols_edit), "")
                     }
-                })
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(MaterialTheme.shapes.large)
+                        .clickable { /* TODO: Show the dialog here :) */ }
+                        .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+                        .padding(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                        Column {
+                            Text("Change Password")
+                            Text("Your password: *********", style = MaterialTheme.typography.bodySmall)
+                        }
+                        Icon(painterResource(R.drawable.material_symbols_edit), "")
+                    }
+                }
+            }
         }
     }
 }
