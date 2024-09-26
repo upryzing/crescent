421f8e4e2a1d5d12c15437079bbdaa5f9d7fbb2a
Bloom <129090449+bloomdevelop@users.noreply.github.com>
2024-09-26 14:18:01 -0400

    chore: upgrade to kotlin 1.9.25 Also `kotlinCompilerExtensionVersion` got upgraded to 1.5.15 It fixes the annoying build errors when trying to test the app as debug release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.15"
     }
     packaging {
         resources {
