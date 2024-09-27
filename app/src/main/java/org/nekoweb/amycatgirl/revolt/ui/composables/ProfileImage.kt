7b56b798e2ff7c367cbace430f4225e3385de1ae
Bloom <129090449+bloomdevelop@users.noreply.github.com>
2024-09-27 12:24:53 -0400

    feat: new ProfileImage now with graphics Took a while to figure it out and it didn't f*ck up the image thingy at least

--- app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/composables/ProfileImage.kt
+++ app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/composables/ProfileImage.kt
@@ -1,9 +1,10 @@
 package org.nekoweb.amycatgirl.revolt.ui.composables
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -14,13 +15,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
 import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
@@ -39,94 +47,98 @@ fun ProfileImage(
     presence: Presence? = null,
     size: Dp = 32.dp
 ) {
-    ConstraintLayout {
-        val (image, presenceIndicator) = createRefs()
-
-        SubcomposeAsyncImage(
-            model = url,
-            contentDescription = "None... yet.",
-            Modifier
-                .constrainAs(image) {
-                    top.linkTo(parent.top)
-                })
-        {
-            val state = painter.state
-            when (state) {
-                is AsyncImagePainter.State.Loading -> {
-                    Box(
-                        Modifier
-                            .clip(CircleShape)
-                            .background(MaterialTheme.colorScheme.primaryContainer)
-                            .padding(5.dp)
-                    ) {
-                        CircularProgressIndicator()
+    SubcomposeAsyncImage(
+        model = url,
+        contentDescription = null,
+        modifier = Modifier
+            .size(size)
+            .aspectRatio(1f)
+            .graphicsLayer {
+                compositingStrategy = CompositingStrategy.Offscreen
+            }
+            .drawWithCache {
+                val path = Path()
+                path.addOval(
+                    Rect(
+                        topLeft = Offset.Zero,
+                        bottomRight = Offset(size.toPx(), size.toPx())
+                    )
+                )
+                onDrawWithContent {
+                    clipPath(path) {
+                        this@onDrawWithContent.drawContent()
                     }
-                }
 
-                is AsyncImagePainter.State.Error,
-                is AsyncImagePainter.State.Empty -> {
-                    Box(
-                        modifier = Modifier
-                            .size(size)
-                            .clip(CircleShape)
-                            .background(MaterialTheme.colorScheme.primary)
-                    ) {
-                        Text(
-                            fallback.substring(0, 1),
-                            modifier = Modifier.align(Alignment.Center),
-                            color = MaterialTheme.colorScheme.onPrimary,
-                            fontSize = 15.sp
+                    val dotSize = size / 5
+                    // Ignore about my bad code bc when is just -> each condition, but I want to check if these are on there.
+                    if (presence == Presence.ONLINE || presence == Presence.FOCUS || presence == Presence.IDLE || presence == Presence.DO_NOT_DISTURB || presence == Presence.OFFLINE) {
+                        drawCircle(
+                            Color.Transparent,
+                            radius = dotSize.toPx(),
+                            center = Offset(
+                                x = size.toPx() - dotSize.toPx() * .8f,
+                                y = size.toPx() - dotSize.toPx() * .8f
+                            ),
+                            blendMode = BlendMode.SrcOut
+                        )
+                        drawCircle(
+                            when (presence) {
+                                Presence.ONLINE -> Green500
+                                Presence.DO_NOT_DISTURB -> Red500
+                                Presence.FOCUS -> Blue500
+                                Presence.IDLE -> Yellow700
+                                Presence.OFFLINE -> Gray400
+                            }, radius = dotSize.toPx() * 0.7f,
+                            center = Offset(
+                                x = size.toPx() - dotSize.toPx() * .8f,
+                                y = size.toPx() - dotSize.toPx() * .8f
+                            )
                         )
                     }
                 }
+            },
+    ) {
+        val state = painter.state
+        when (state) {
+            is AsyncImagePainter.State.Loading -> {
+                Column(
+                    Modifier
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .padding(5.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top
+                ) {
+                    CircularProgressIndicator(Modifier.size(5.dp))
+                }
+            }
 
-                is AsyncImagePainter.State.Success -> {
-                    SubcomposeAsyncImageContent(
-                        modifier = Modifier
-                            .size(size)
-                            .clip(CircleShape)
-                            .background(MaterialTheme.colorScheme.primaryContainer),
-                        contentScale = ContentScale.FillBounds
+            is AsyncImagePainter.State.Error,
+            is AsyncImagePainter.State.Empty -> {
+                Box(
+                    modifier = Modifier
+                        .size(size)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primary)
+                ) {
+                    Text(
+                        fallback.substring(0, 1),
+                        modifier = Modifier.align(Alignment.Center),
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        fontSize = 15.sp
                     )
                 }
             }
-        }
-        AnimatedVisibility(
-            presence != null,
-            modifier = Modifier
-                .constrainAs(presenceIndicator) {
-                    bottom.linkTo(image.bottom, margin = (-3).dp)
-                    end.linkTo(image.end, margin = (-3).dp)
-                }
-                .graphicsLayer {
-                    clip = true
-                    shape = CircleShape
-                }
-        ) {
-            Box(
-                // Weird workaround for this "effect"
-                // TODO: find a way to cut off to create interesting effect just like in SF Symbols by Apple.
-                // I know the way I'm doing with "graphics", but it will be painful due how it was programmed :,)
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colorScheme.background)
-                    .padding(2.dp)
-                    .clip(CircleShape)
-                    .background(
-                        when (presence) {
-                            Presence.ONLINE -> Green500
-                            Presence.DO_NOT_DISTURB -> Red500
-                            Presence.FOCUS -> Blue500
-                            Presence.IDLE -> Yellow700
-                            Presence.OFFLINE -> Gray400
-                            else -> {
-                                Gray400
-                            }
-                        }
-                    )
-                    .size(size / 4 * 1.2f)
 
-            )
+            is AsyncImagePainter.State.Success -> {
+                SubcomposeAsyncImageContent(
+                    modifier = Modifier
+                        .size(size)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primaryContainer),
+                    contentScale = ContentScale.FillBounds
+                )
+            }
         }
     }
 }
