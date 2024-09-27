2a3711a953edbb7fa3637e1f39919f4d5f81adf2
Bloom <129090449+bloomdevelop@users.noreply.github.com>
2024-09-27 12:57:16 -0400

    chore(chat): remove unused import

--- app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/navigation/Chat.kt
+++ app/src/main/java/org/nekoweb/amycatgirl/revolt/ui/navigation/Chat.kt
@@ -5,7 +5,6 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
