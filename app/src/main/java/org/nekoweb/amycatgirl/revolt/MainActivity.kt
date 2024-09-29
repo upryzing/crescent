46d8533cfd7eac4995459597e00bd7617d80f95f
Bloom <129090449+bloomdevelop@users.noreply.github.com>
2024-09-29 18:46:16 -0400

    fix(MainActivity): remove Color.Transparent seems like it's unnecessary since it makes worse on light mode
    Why Color.Transparent made dark mode so superior to light mode???

--- app/src/main/java/org/nekoweb/amycatgirl/revolt/MainActivity.kt
+++ app/src/main/java/org/nekoweb/amycatgirl/revolt/MainActivity.kt
@@ -1,9 +1,7 @@
 package org.nekoweb.amycatgirl.revolt
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import org.nekoweb.amycatgirl.revolt.ui.theme.RevoltTheme
@@ -11,7 +9,7 @@ import org.nekoweb.amycatgirl.revolt.ui.theme.RevoltTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT))
+        enableEdgeToEdge()
 
         setContent {
             RevoltTheme {
