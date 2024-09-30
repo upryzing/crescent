eecc992d0789f362d0f75ab658723bf68208f557
Bloom <129090449+bloomdevelop@users.noreply.github.com>
2024-09-30 09:17:04 -0400

    feat(layout): add adaptive dependencies required for tablet or "fold phone"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,7 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.emoji.java)
+    implementation(libs.androidx.window)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
