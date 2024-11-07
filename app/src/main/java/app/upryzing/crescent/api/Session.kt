416e6708ac74adcbe1247dc0c01955ecae787fbb
Amy <amy@amogus.cloud>
2024-11-06 22:10:25 -0400

    chore: implement createSession in Session subclass

--- app/src/main/java/app/upryzing/crescent/api/Session.kt
+++ app/src/main/java/app/upryzing/crescent/api/Session.kt
@@ -1,11 +1,27 @@
 package app.upryzing.crescent.api
 
+import app.upryzing.crescent.models.api.authentication.EmailSessionRequest
 import app.upryzing.crescent.models.api.authentication.SessionResponse
+import io.ktor.client.request.accept
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 
-class Session(val client: RevoltAPI) {
-    val currentSession: SessionResponse.Success? = null;
+class Session(private val client: RevoltAPI) {
+    var currentSession: SessionResponse.Success? = null;
 
-    fun createSession() {
+    suspend fun createSession(email: String, password: String): SessionResponse {
+        val response = client.http.post<SessionResponse>("${client.options.apiUrl}/auth/session/login") {
+            accept(ContentType.Application.Json)
+            contentType(ContentType.Application.Json)
 
+            setBody(EmailSessionRequest(email, password))
+        }
+
+        if (response is SessionResponse.Success) {
+            currentSession = response
+        }
+
+        return response
     }
 }
