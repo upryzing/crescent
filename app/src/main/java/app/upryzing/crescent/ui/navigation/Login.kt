30e912ad3f5002baf5a22075782db00e046a1d7f
Bloom <bloomdevelop@proton.me>
2025-01-14 08:38:17 -0400

    fix(login): sp is not imported

--- app/src/main/java/app/upryzing/crescent/ui/navigation/Login.kt
+++ app/src/main/java/app/upryzing/crescent/ui/navigation/Login.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.upryzing.crescent.models.viewmodels.LoginViewmodel
 import app.upryzing.crescent.ui.composables.AccountDisabledDialog
 import kotlinx.serialization.Serializable
