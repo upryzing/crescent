88558878d30a209aee74d90fd8f138938f4481c3
Bloom <bloomdevelop@proton.me>
2025-01-15 09:23:51 -0400

    wip(ChatBubble): Context Action + Reactions (everything is placeholder rn) When implementation? :trollface:

--- app/src/main/java/app/upryzing/crescent/ui/composables/ChatBubble.kt
+++ app/src/main/java/app/upryzing/crescent/ui/composables/ChatBubble.kt
@@ -1,21 +1,47 @@
 package app.upryzing.crescent.ui.composables
 
 import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Square
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,12 +51,19 @@ import app.upryzing.crescent.models.api.User
 import app.upryzing.crescent.models.api.websocket.PartialMessage
 import app.upryzing.crescent.ui.theme.RevoltTheme
 import com.vdurmont.emoji.EmojiParser
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ChatBubble(message: PartialMessage, modifier: Modifier = Modifier, isSelf: Boolean = false) {
     val author = remember(message.authorId) {
         ApiClient.cache[message.authorId] as User?
     }
+
+    // BottomSheet states
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
     fun checkIfMessageOnlyContainsEmoji(): Boolean {
         if (message.content == null) return false
@@ -59,15 +92,62 @@ fun ChatBubble(message: PartialMessage, modifier: Modifier = Modifier, isSelf: B
             color = if (isOnlyEmoji)
                 Color.Transparent else if (isSelf)
                 MaterialTheme.colorScheme.primaryContainer else MaterialTheme.colorScheme.surfaceContainerHigh,
-            shape = RoundedCornerShape(18.dp)
-        ) {
-            SelectionContainer {
-                Text(
-                    text = if (!message.content.isNullOrBlank()) message.content else if (message.attachments != null) "Message has no content, but it has attachments, and we can't render them yet." else "Unhandled",
-                    color = if (isSelf) MaterialTheme.colorScheme.onPrimaryContainer else MaterialTheme.colorScheme.onSurface,
-                    fontSize = if (isOnlyEmoji) 48.sp else 16.sp,
-                    modifier = Modifier.padding(10.dp)
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.large)
+                .combinedClickable(
+                    onClick = { /* NOTE: Leave them empty for now */ },
+                    onLongClick = {
+                        Log.d("ChatBubble", "Long Pressed")
+                        showBottomSheet = true
+                    }
                 )
+        ) {
+            Text(
+                text = if (!message.content.isNullOrBlank()) message.content else if (message.attachments != null) "Message has no content, but it has attachments, and we can't render them yet." else "Unhandled",
+                color = if (isSelf) MaterialTheme.colorScheme.onPrimaryContainer else MaterialTheme.colorScheme.onSurface,
+                fontSize = if (isOnlyEmoji) 48.sp else 16.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+        }
+    }
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                showBottomSheet = false
+            },
+            sheetState = sheetState
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                repeat(5) {
+                    Column(
+                        Modifier
+                            .clip(CircleShape)
+                            .background(MaterialTheme.colorScheme.surfaceContainerHigh)
+                            .clickable {
+                                /* TODO: Do something! */
+                                scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                    if (!sheetState.isVisible) {
+                                        showBottomSheet = false
+                                    }
+                                }
+                            }
+                            .padding(10.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Icon(Icons.Default.Square, "Not Localized yet", Modifier.size(34.dp))
+                    }
+                }
+            }
+            repeat(5) {
+                ListItem(
+                    headlineContent = { Text("Item") },
+                    leadingContent = { Icon(Icons.Default.Square, "Not Localized yet") },
+                    modifier = Modifier.clickable { /* TODO: Do something! */ })
             }
         }
     }
