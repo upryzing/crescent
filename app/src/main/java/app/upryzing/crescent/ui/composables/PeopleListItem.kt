df31d9b07d8afb9cda75b480cb7f5713cb29a28f
Bloom <bloomdevelop@proton.me>
2025-01-15 09:46:22 -0400

    wip(PeopleListItem): add bottom sheet for actions For example: "Block" action, "Report User" action and more

--- app/src/main/java/app/upryzing/crescent/ui/composables/PeopleListItem.kt
+++ app/src/main/java/app/upryzing/crescent/ui/composables/PeopleListItem.kt
@@ -1,23 +1,47 @@
 package app.upryzing.crescent.ui.composables
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Square
 import androidx.compose.material3.Badge
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import app.upryzing.crescent.api.ApiClient
 import app.upryzing.crescent.models.api.User
 import app.upryzing.crescent.models.api.UserStatus
 import app.upryzing.crescent.models.api.channels.Channel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun PeopleListItem(
     user: User? = null,
@@ -26,7 +50,15 @@ fun PeopleListItem(
     unreads: Int? = null,
     callback: (() -> Unit)
 ) {
-    Surface(onClick = { callback() }) {
+    // BottomSheet states
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+    Surface(modifier = Modifier.combinedClickable(
+        onClick = callback,
+        onLongClick = { showBottomSheet = true}
+    )) {
         Column {
             val name = if (user != null) {
                 user.displayName ?: "${user.username}#${user.discriminator}"
@@ -85,6 +117,21 @@ fun PeopleListItem(
                 }
             )
             HorizontalDivider(modifier = Modifier.padding(start = 16.dp, end = 16.dp))
+        }
+    }
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                showBottomSheet = false
+            },
+            sheetState = sheetState
+        ) {
+            repeat(5) {
+                ListItem(
+                    headlineContent = { Text("Item") },
+                    leadingContent = { Icon(Icons.Default.Square, "Not Localized yet") },
+                    modifier = Modifier.clickable { /* TODO: Do something! */ })
+            }
         }
     }
 }
