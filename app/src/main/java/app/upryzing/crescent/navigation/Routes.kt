e178d11a2e98dd17c807910e838badb823bf238b
Bloom Perez <springdotgay@tuta.io>
2025-09-04 21:10:33 -0400

    chore(NavHost): forgot to include Routes.kt

---
+++ app/src/main/java/app/upryzing/crescent/navigation/Routes.kt
@@ -0,0 +1,21 @@
+package app.upryzing.crescent.navigation
+
+object Routes {
+    const val DEBUG = "debug"
+    const val AUTH = "auth"
+    const val HOME = "home"
+    const val START_CONVERSATION = "home/start_conversation"
+
+    object Messages {
+        const val ROUTE_PATTERN = "messages/{id}"
+        fun destination(id: String) = "messages/$id"
+    }
+
+    const val SETTINGS = "settings"
+    object SettingsSubRoutes {
+        const val ACCOUNT = "settings/account"
+        const val PROFILE = "settings/profile"
+        const val CLIENT = "settings/client"
+        const val ABOUT = "settings/about"
+    }
+}
